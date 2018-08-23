b44ca6f27606dd08e9b395002647752f2052b3fc
Jenkins <systems@yapily.com>
2018-08-23 11:32:55 +0000

    latest version

--- examples/example_account_details.py
+++ examples/example_account_details.py
@@ -22,7 +22,7 @@ def main():
     user_api = ApplicationUsersApi(apiClient)
 
     application_user = ApplicationUser()
-    application_user._app_user_id = str(uuid.uuid4())
+    application_user._reference_id = str(uuid.uuid4())
 
     created_user = user_api.add_user_using_post_with_http_info(application_user)[0]
 
@@ -31,7 +31,7 @@ def main():
     institution_id = "bbva-sandbox";
     app_user_uuid = created_user._uuid
 
-    redirect_url = auth_direct_url(constants.APPLICATION_ID,app_user_uuid,institution_id,constants.CALLBACK_URL,"account")
+    redirect_url = auth_direct_url(constants.APPLICATION_ID,app_user_uuid,institution_id,create_callback_with_user_uuid(created_user._uuid),"account")
     webbrowser.open(redirect_url)
 
     input("Press enter to continue")
@@ -39,7 +39,7 @@ def main():
     consent = ConsentsApi(apiClient).get_user_consents_using_get(app_user_uuid)[0]
     print("Consent: " + consent.consent_token);
     identity_api = IdentityApi(ApiClient(configuration))
-    identity =  identity_api.identity_using_get(consent.consent_token)
+    identity =  identity_api.get_identity_using_get(consent.consent_token)
 
     print("**************IDENTITY******************")
     print(identity)
@@ -57,6 +57,9 @@ def main():
     print("**************TRANSACTIONS**************");
     print(transactions);
     print("****************************************");
+
+def create_callback_with_user_uuid(user_uuid):
+    return constants.CALLBACK_URL+ "?user_uuid="+user_uuid
 
 if __name__ == '__main__':
     main()
--- examples/example_configure_users.py
+++ examples/example_configure_users.py
@@ -17,41 +17,25 @@ def main():
 
     user_api = ApplicationUsersApi(apiClient)
     user_api.add_user_using_post(new_application_user())
-    user_api.add_user_using_post_with_http_info(new_application_user())
 
-    application_user_c = new_application_user()
-    user_api.add_user_using_post_with_http_info(new_application_user())
-
-    print("Created Users")
+    print("Created User")
 
     application_users = user_api.get_users_using_get()
 
     print(application_users)
 
-    user_uuid = application_users[0]._uuid
+    user_api.delete_user_using_delete(application_users[-1]._uuid)
 
-    user_api.get_user_using_get_with_http_info(user_uuid)
+    for user in user_api.get_users_using_get():
+        print(user._uuid)
 
-    for application_user in application_users:
-        print(application_user._app_user_id)
-
-    application_user_c._app_user_id = "UserC"
-
-    upd = user_api.update_user_using_put(str(application_user_c._uuid),application_user_c)
-
-    print("Updated application user")
-
-    updated_application_users = user_api.get_users_using_get()
-
-    for application_user in updated_application_users:
-        print(application_user._app_user_id)
 
 def new_application_user():
-    application_user = NewApplicationUser()
-    application_user.reference_id = str(uuid.uuid4())
+    application_user = NewApplicationUser(str(uuid.uuid4()))
     return application_user
 
 
 if __name__ == '__main__':
     main()
+
 
