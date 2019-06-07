a779806e8048bb11ad549fa6da6d154b5d28d14a
Jenkins <systems@yapily.com>
2019-06-07 17:42:53 +0000

    latest version

--- sdk/yapily/models/balance.py
+++ sdk/yapily/models/balance.py
@@ -72,6 +72,12 @@ class Balance(object):
         :param type: The type of this Balance.  # noqa: E501
         :type: str
         """
+        allowed_values = ["CLOSING_AVAILABLE", "CLOSING_BOOKED", "CLOSING_CLEARED", "EXPECTED", "FORWARD_AVAILABLE", "INFORMATION", "INTERIM_AVAILABLE", "INTERIM_BOOKED", "INTERIM_CLEARED", "OPENING_AVAILABLE", "OPENING_BOOKED", "OPENING_CLEARED", "PREVIOUSLY_CLOSED_BOOKED", "AUTHORISED", "OTHER", "UNKNOWN"]  # noqa: E501
+        if type not in allowed_values:
+            raise ValueError(
+                "Invalid value for `type` ({0}), must be one of {1}"  # noqa: E501
+                .format(type, allowed_values)
+            )
 
         self._type = type
 
