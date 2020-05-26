868e682260cbd6ead4c8da0115a2a3be02a2f0c2
yapily-mofe <57627291+yapily-mofe@users.noreply.github.com>
2020-05-26 19:25:28 +0100

    Update constants.py

--- examples/constants.py
+++ examples/constants.py
@@ -1,5 +1,5 @@
-APPLICATION_ID = "54716dbc-36af-472a-b3ad-e6356c75c88d"
-APPLICATION_SECRET = "e5738fc6-fe3f-4634-b00b-13a263643b10"
-CALLBACK_URL = "http://display-parameters.herokuapp.com/" # Add a callback 
+APPLICATION_ID = "" # Add your Yapily application key (NOT SUITABLE FOR PRODUCTION)
+APPLICATION_SECRET = "" # Add your Yapily application secret (NOT SUITABLE FOR PRODUCTION)
+CALLBACK_URL = "" # Add a callback 
 APPLICATION_USER_ID = "python-sdk@yapily.com"
-INSTITUTION_ID = "modelo-sandbox" # Replace this with any institution Id that exists in your application
+INSTITUTION_ID = "modelo-sandbox" # Replace this with any institution Id that exists in your Yapily application
